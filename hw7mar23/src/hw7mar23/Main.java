96c5a46b13dc0b971c2124a326016e0459708c60
Bohdan <notpadumink01@gmail.com>
2023-03-07 17:53:51 +0200

    hi

--- hw7mar23/src/hw7mar23/Main.java
+++ hw7mar23/src/hw7mar23/Main.java
@@ -1,16 +1,40 @@
 package hw7mar23;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 	public static void main(String[] args) {
-		int[] array = {4,1,2,5,3};
-		System.out.println(getIndByNum(array, 6));
-	}
-	
-	public static int getIndByNum(int[] array, int num) {
-		for(int i=0; i<array.length; i++) {
-			if (array[i] == num)
-				return i;
+		int l = 15;
+		int[] array = new int[l];
+		
+		for (int i = 0; i<l; i++) {
+			array[i] = i;
 		}
-		return -1;
+		
+		System.out.println(Arrays.toString(array));
+		
+		array[1] = 0;
+		int m = 2;
+		
+		while (m<l) {
+			if(array[m] != 0) {
+				int j = m*2;
+				while(j<l) {
+					array[j]=0;
+					j += m;
+				}
+			}
+			m++;
+		}
+		List<Integer> myList = new ArrayList<>();
+		
+		for (int i : array) {
+			if (i != 0)
+				myList.add(i);
+		}
+		System.out.println(Arrays.toString(myList.toArray()));
+		
 	}
 }
